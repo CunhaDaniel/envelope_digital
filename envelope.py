687d8093999ad751a75c7a1b626d59402e24e785
Daniel Cunha <daniel.cunha@nuuvem.com>
2023-11-27 23:39:34 -0300

    Valid signature flow

--- envelope.py
+++ envelope.py
@@ -44,9 +44,8 @@ def create_envelope(plain_text, public_key_dest, private_key_remet, encryp_algor
     encrypted_message = cipher.encrypt(plain_text)
 
     private_key = RSA.import_key(open(private_key_remet).read())
-    h = SHA256.new(encrypted_message)
-    signature = pkcs1_15.new(private_key).sign(h)
 
+    
     instantiate_dest_public_key = RSA.import_key(open(public_key_dest).read())
     cipher_rsa                  = PKCS1_OAEP.new(instantiate_dest_public_key)
     encrypt_session_key         = cipher_rsa.encrypt(session_key)
@@ -54,7 +53,36 @@ def create_envelope(plain_text, public_key_dest, private_key_remet, encryp_algor
     output_message = open("./tmp/messages/message", "wb")
     output_key     = open("./tmp/messages/key", "wb")
 
-    output_message.write(signature)
+    output_message.write(encrypted_message)
     output_key.write(encrypt_session_key)
 
+    output_message.close()
+    output_key.close()
+    print(encrypted_message)
+    h = SHA256.new(open("./tmp/messages/message", "rb").read())
+    signature = pkcs1_15.new(private_key).sign(h)
+    print(signature)
+    open('./tmp/messages/signature', 'wb').write(signature)
+    
 create_envelope(plain_text, public_key_dest, private_key_remet, encryp_algorithm)
+
+message = "tmp/messages/message"
+signature_path = "tmp/messages/signature"
+public_key_remet = "tmp/public/public_key_bob.pem"
+
+def open_envelope(message, public_key_remet):
+    data = open(message, 'rb').read()
+    print(data)
+    key = RSA.import_key(open(public_key_remet).read())
+    signature = open(signature_path, 'rb').read()
+    print(signature)
+    h = SHA256.new(data)
+    try:
+        pkcs1_15.new(key).verify(h, signature)
+        print("Assinatura válida.")
+        return True
+    except (ValueError, TypeError):
+        print("Assinatura inválida.")
+        return False
+
+open_envelope(message, public_key_remet)
